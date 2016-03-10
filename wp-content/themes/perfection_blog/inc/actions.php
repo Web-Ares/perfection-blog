8ee161f778dfe00151e9f4651548dbca24c81375
iutin <evgeniy.iutin@gmail.com>
2016-03-10 12:00:09 +0200

    clear theme

--- wp-content/themes/perfection_blog/inc/actions.php
+++ wp-content/themes/perfection_blog/inc/actions.php
@@ -1,11 +1,13 @@
 <?php
-
+//required actions
 remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'feed_links', 2);
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'feed_links_extra', 3);
 remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'noindex');
+add_filter('xmlrpc_enabled','__return_false');
+remove_action('wp_head', 'wlwmanifest_link');
+// close required actions
+
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'feed_links_extra', 3);
 remove_action('wp_head', 'rel_canonical');
 remove_action('wp_head', 'signuppageheaders');
 
