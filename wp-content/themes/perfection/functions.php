e3b7278a1dd14c62801f716229cb779d4baa1ad1
iutin <evgeniy.iutin@gmail.com>
2016-03-14 16:29:45 +0200

    some result

--- wp-content/themes/perfection/functions.php
+++ wp-content/themes/perfection/functions.php
@@ -11,7 +11,7 @@ $blog_info = get_bloginfo('template_url');
 define('BLOGINFO', $blog_info);
 remove_filter('template_redirect', 'redirect_canonical');
 
-//show_admin_bar( false );
+show_admin_bar( false );
 // Load library files.
 require_once( TEMPLATEINC . '/shortcodes.php' );
 require_once( TEMPLATEINC . '/cpt.php' );
