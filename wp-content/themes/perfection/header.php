e21c7a4c40778468f1304de9ea839338dd75bca0
iutin <evgeniy.iutin@gmail.com>
2016-03-22 10:26:33 +0200

    change logo link

--- wp-content/themes/perfection/header.php
+++ wp-content/themes/perfection/header.php
@@ -9,7 +9,7 @@
     <title><?php document_title(); ?></title>
     <?php wp_head(); ?>
     <style>
-        .preloader{
+        .preloader {
             position: fixed;
             top: 0;
             left: 0;
@@ -22,7 +22,8 @@
             -webkit-transition: visibility .3s ease, opacity .3s ease;
             z-index: 4;
         }
-        .preloader_hide{
+
+        .preloader_hide {
             visibility: hidden;
             opacity: 0;
         }
@@ -32,13 +33,22 @@
 
 <body>
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W2BZ82"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-W2BZ82');
+<noscript>
+    <iframe src="//www.googletagmanager.com/ns.html?id=GTM-W2BZ82"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<script>(function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(), event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-W2BZ82');
 </script>
 
 <!-- End Google Tag Manager -->
@@ -55,24 +65,16 @@
         <!-- site__header-layout -->
         <div class="site__header-layout">
 
-            <?php if(is_front_page()){?>
-                <!-- logo -->
-                <h1 class="logo">
-                    <img src="<?php echo TEMPLATEURI ?>/img/logo-small.png?<?php echo time();?>" alt="logo">
-                </h1>
-                <!-- /logo -->
-            <?php }else{?>
-                <!-- logo -->
-                <a href="<?php home_url(); ?>" class="logo">
-                    <img src="<?php echo TEMPLATEURI ?>/img/logo-small.png?<?php echo time();?>" alt="logo">
-                </a>
-                <!-- /logo -->
-            <?php }?>
+            <a href="<?php home_url(); ?>" class="logo">
+                <img src="<?php echo TEMPLATEURI ?>/img/logo-small.png?<?php echo time(); ?>" alt="logo">
+            </a>
+            <!-- /logo -->
+
             <!-- header-menu__mobile-item -->
             <a href="/view-icons/" class="header-menu__mobile-item">ICONS</a>
             <!-- /header-menu__mobile-item -->
 
-            <?php echo get_visible_menu($post->ID);?>
+            <?php echo get_visible_menu($post->ID); ?>
 
             <!-- site__header__menu-drop -->
             <a class="drop-menu-btn" href="#"><span></span></a>
@@ -88,21 +90,21 @@
             <div class="drop-menu__inner" id="">
 
                 <!-- drop-menu__inner-wrap -->
-                <div class="drop-menu__inner-wrap"  id="scroll-wrap" >
+                <div class="drop-menu__inner-wrap" id="scroll-wrap">
 
                     <!-- scroller -->
-                    <div  id="scroller">
+                    <div id="scroller">
 
                         <!-- drop-menu__menu -->
-                       <?php echo get_menus($post->ID,'top_menu'); ?>
+                        <?php echo get_menus($post->ID, 'top_menu'); ?>
                         <!-- /drop-menu__menu -->
 
                         <!-- drop-menu__navigation -->
-                        <?php echo get_menus($post->ID,'middle_menu'); ?>
+                        <?php echo get_menus($post->ID, 'middle_menu'); ?>
                         <!-- /drop-menu__navigation -->
 
                         <!-- drop-menu__legally -->
-                        <?php echo get_menus($post->ID,'bottom_menu'); ?>
+                        <?php echo get_menus($post->ID, 'bottom_menu'); ?>
                         <!-- /drop-menu__legally -->
 
                     </div>
@@ -123,7 +125,8 @@
                 <a href="#" class="social__item social__item_mail adoric_f2f0a9ee"></a>
                 <a href="https://dribbble.com/icon54" target="_blank" class="social__item social__item_btn"></a>
                 <a href="https://twitter.com/54_icon" target="_blank" class="social__item social__item_tw"></a>
-                <a href="https://www.facebook.com/Icon54-1762416167312152/?fref=ts" target="_blank" class="social__item social__item_fb"></a>
+                <a href="https://www.facebook.com/Icon54-1762416167312152/?fref=ts" target="_blank"
+                   class="social__item social__item_fb"></a>
             </div>
             <!-- /social -->
 
